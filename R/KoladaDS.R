be36b1649e4c268c48a165d5c98017369c28106e
aswma317 <aswma317@student.liu.se>
2022-09-22 23:12:36 +0200

    Included KoladaDS.R under R folder. This contains the function get_kolada_data.

---
+++ R/KoladaDS.R
@@ -0,0 +1,26 @@
+library(httr)
+library(jsonlite)
+library(tidyr)
+get_kolda_data <- function(url_path){
+  # base_url <- "https://api.kolada.se/v2/data/kpi/n60026/year/2020,2019,2018" #Total Monthly salary
+  base_url <- "https://api.kolada.se/v2/data/"
+  final_url <- paste(base_url, url_path, sep = "")
+  
+  #Call the Request
+  kolada_response <- GET(final_url)
+  
+  #Check if the response is of the JSON type
+  if(http_type(kolada_response) != "application/json")
+    stop("Response not in JSON")
+  
+  #Parse the response
+  parsed <- jsonlite::fromJSON(content(kolada_response, "text"),
+                               simplifyVector = FALSE)
+  api_char <- base::rawToChar(kolada_response$content)
+  api_JSON <- jsonlite::fromJSON(api_char)
+  #Unnest the values list which contains data frames
+  output_tibble <- unnest(as_tibble(api_JSON$values),cols = c('values'))
+  return(as.data.frame(output_tibble))
+}
+
+get_kolda_data('kpi/n60026/year/2020,2019,2018')
