4d3f445d36ab5e6153c49635cc477b34cb304920
aswma317 <aswma317@student.liu.se>
2022-09-23 12:21:10 +0200

    Included error handling for bad http request

--- R/KoladaDS.R
+++ R/KoladaDS.R
@@ -1,6 +1,6 @@
-library(httr)
-library(jsonlite)
-library(tidyr)
+# library(httr)
+# library(jsonlite)
+# library(tidyr)
 get_kolda_data <- function(url_path){
   # base_url <- "https://api.kolada.se/v2/data/kpi/n60026/year/2020,2019,2018" #Total Monthly salary
   base_url <- "https://api.kolada.se/v2/data/"
@@ -8,7 +8,7 @@ get_kolda_data <- function(url_path){
   
   #Call the Request
   kolada_response <- GET(final_url)
-  
+
   #Check if the response is of the JSON type
   if(http_type(kolada_response) != "application/json")
     stop("Response not in JSON")
@@ -16,6 +16,14 @@ get_kolda_data <- function(url_path){
   #Parse the response
   parsed <- jsonlite::fromJSON(content(kolada_response, "text"),
                                simplifyVector = FALSE)
+  
+  #Check if the response is good
+  if(http_error(kolada_response)){
+    stop(paste("The request errored out with the status:",
+               kolada_response$status_code, 
+               ".\n Error Message:", parsed$message))
+  }
+  
   api_char <- base::rawToChar(kolada_response$content)
   api_JSON <- jsonlite::fromJSON(api_char)
   #Unnest the values list which contains data frames
